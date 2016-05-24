3f0bb72eee3f217a4bd5649a98a5be79cceb4f93
djanny22 <janbocko@gmail.com>
2016-05-24 15:41:47 +0200

    Add files via upload

---
+++ DSC.ps1
@@ -0,0 +1,45 @@
+﻿Configuration homelab{
+
+    node ADDS{
+        Import-DscResource –ModuleName @{ModuleName="xActiveDirectory";ModuleVersion="2.11.0.0"}
+
+        $HostData = $ConfigurationData.DC
+        $DomainCred = Get-AutomationPSCredential -Name "DA Credentials"
+        $SafemodeAdministratorPassword = Get-AutomationPSCredential -Name "ADSafeMode"
+        $NewADUserCred = = Get-AutomationPSCredential -Name "Defaul AD Pwd"
+
+        WindowsFeature ADDSInstall
+        {
+            Ensure = "Present"
+            Name = "AD-Domain-Services"
+        }
+
+        xADDomain FirstDS
+        {
+            DomainName = $HostData.DomainName
+            DomainAdministratorCredential = $DomainCred 
+            SafemodeAdministratorPassword = $SafemodeAdministratorPassword 
+            DependsOn = "[WindowsFeature]ADDSInstall"
+        }
+
+        xWaitForADDomain DscForestWait
+        {
+            DomainName = $HostData.DomainName
+            DomainUserCredential = $domainCred
+            RetryCount = $HostData.RetryCount
+            RetryIntervalSec = $HostData.RetryIntervalSec
+            DependsOn = "[xADDomain]FirstDS"
+        }
+        xADUser FirstUser
+        {
+            DomainName = $HostData.DomainName
+            DomainAdministratorCredential = $domainCred
+            UserName = "dummy"
+            Password = $NewADUserCred
+            Ensure = "Present"
+            DependsOn = "[xWaitForADDomain]DscForestWait"
+        }
+
+
+    }
+}
